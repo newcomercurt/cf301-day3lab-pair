c0ed368786332f906ccb51b08fc941e13b3fd479
mericks <mericks@users.noreply.github.com>
2016-06-08 16:24:15 -0700

    completed articleView.handleAuthorFilter = function()

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -29,13 +29,23 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      // DONE: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      var selectedAuthor = $(this).val();
+      $('article').each(function() {
+        $(this).data('author');
+        var author = $(this).data('author');
+        if (author === selectedAuthor) {
+          $(this).fadeIn('slow');
+        } else {
+          $(this).hide();
+        }
+      });
+  // DONE: If the select box was changed to an option that is blank, we should
+  //       show all the articles, except the one article we are using as a template.
     } else {
-      // TODO: If the select box was changed to an option that is blank, we should
-      //       show all the articles, except the one article we are using as a template.
+      $('article').show();
 
     }
     $('#category-filter').val('');
@@ -73,4 +83,6 @@ articleView.setTeasers = function() {
 };
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
+articleView.populateFilters();
+articleView.handleAuthorFilter();
 $();
